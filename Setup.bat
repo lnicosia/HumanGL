9906c96549d425ef604531e143defc1bd9611dc5
lnicosia <lnicosia@student.42.fr>
2021-11-11 14:36:46 +0100

    [FIX] Setup.bat call order is now correct, will not skip the .dll copy if it had to be built anymore.

--- Setup.bat
+++ Setup.bat
@@ -5,26 +5,26 @@ if not exist lib\notrealengine\.git (
 	git submodule update --init lib/notrealengine
 	)
 
-	
-if not exist lib\SDL\include\SDL.h (
-	git submodule update --init lib/SDL
+if not exist SDL2d.dll (
+
+	if not exist lib\SDL\build-windows\Debug\SDL2d.dll (
+
+		if not exist lib\SDL\build-windows\SDL2.sln (
+
+			if not exist lib\SDL\include\SDL.h (
+				git submodule update --init lib/SDL
+			)
+			if not exist lib\SDL\build-windows (
+				md lib\SDL\build-windows
+			)
+			cmake -S lib\SDL\ -B lib\SDL\build-windows
+			
+		)
+		for /f "delims=" %%i in ('lib\vswhere.exe -version 16 -property installationPath -nologo') do (
+			set msbuild-path="%%i\MSBuild\Current\Bin\MSBuild.exe"
+			echo Launching !msbuild-path!...
+			!msbuild-path! lib\SDL\build-windows\SDL2.sln
+		)
 	)
-	
-if not exist SDL2d.dll goto MoveSDL	
-
-:MoveSDL
-
-if not exist lib\SDL\build-windows\Debug\SDL2d.dll goto BuildSDL
-move lib\SDL\build-windows\Debug\SDL2d.dll .
-
-:BuildSDL
-
-md lib\SDL\build-windows
-if not exist lib\SDL\build-windows\SDL2.sln (
-	cmake -S lib\SDL\ -B lib\SDL\build-windows
-	)
-for /f "delims=" %%i in ('lib\vswhere.exe -version 16 -property installationPath -nologo') do (
-	set msbuild-path="%%i\MSBuild\Current\Bin\MSBuild.exe"
-	echo Launching !msbuild-path!...
-	!msbuild-path! lib\SDL\build-windows\SDL2.sln
-	)
+	move lib\SDL\build-windows\Debug\SDL2d.dll .
+)
