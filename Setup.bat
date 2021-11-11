328fdd98c1b79fac1d61f4c5c7e521ee1b34b255
lnicosia <lnicosia@student.42.fr>
2021-11-11 09:13:32 +0100

    [ADD] Windows install script Setup.bat, VS solution and vswhere.exe added to make the project usable on Windows for VS 2019.

---
+++ Setup.bat
@@ -0,0 +1,30 @@
+@echo off
+SetLocal EnableDelayedExpansion
+
+if not exist lib\notrealengine\.git (
+	git submodule update --init lib/notrealengine
+	)
+
+	
+if not exist lib\SDL\include\SDL.h (
+	git submodule update --init lib/SDL
+	)
+	
+if not exist SDL2d.dll goto MoveSDL	
+
+:MoveSDL
+
+if not exist lib\SDL\build-windows\SDL2.dll goto BuildSDL
+move lib\SDL\build-windows\SDL2d.dll .
+
+:BuildSDL
+
+md lib\SDL\build-windows
+if not exist lib\SDL\build-windows\SDL2.sln (
+	cmake -S lib\SDL\ -B lib\SDL\build-windows
+	)
+for /f "delims=" %%i in ('lib\vswhere.exe -version 16 -property installationPath -nologo') do (
+	set msbuild-path="%%i\MSBuild\Current\Bin\MSBuild.exe"
+	echo Launching !msbuild-path!...
+	!msbuild-path! lib\SDL\build-windows\SDL2.sln
+	)
