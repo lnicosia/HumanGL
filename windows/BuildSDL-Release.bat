accf03fd36d0796e646e65d0afc0a4ef2b3791ce
lnicosia <lnicosia@student.42.fr>
2022-05-26 10:06:09 +0200

    [ADD] Windows uses SDL from lib/notrealengine instead of root SDL/.

--- windows/BuildSDL-Release.bat
+++ windows/BuildSDL-Release.bat
@@ -1,20 +1,20 @@
 @echo off
 SetLocal EnableDelayedExpansion
 
-if not exist ..\lib\SDL\build-windows\Release\SDL2.lib (
+if not exist ..\lib\notrealengine\lib\SDL\build-windows\Release\SDL2.lib (
 
-	if not exist ..\lib\SDL\include\SDL.h (
-		git submodule update --init ..\lib\SDL
+	if not exist ..\lib\notrealengine\lib\SDL\include\SDL.h (
+		git submodule update --init ..\lib\notrealengine\lib\SDL
 	)
-	if not exist ..\lib\SDL\build-windows (
-		md ..\lib\SDL\build-windows
+	if not exist ..\lib\notrealengine\lib\SDL\build-windows (
+		md ..\lib\notrealengine\lib\SDL\build-windows
 	)
-	cmake -S ..\lib\SDL\ -B ..\lib\SDL\build-windows
-	cmake --build ..\lib\SDL\build-windows --config Release
-	cp ..\lib\SDL\build-windows\Release\SDL2.dll x64\Release
+	cmake -S ..\lib\notrealengine\lib\SDL\ -B ..\lib\notrealengine\lib\SDL\build-windows
+	cmake --build ..\lib\notrealengine\lib\SDL\build-windows --config Release
+	cp ..\lib\notrealengine\lib\SDL\build-windows\Release\SDL2.dll x64\Release
 
 )
 
 if not exist x64\Release\SDL2.dll  (
-	copy ..\lib\SDL\build-windows\Release\SDL2.dll x64\Release
+	copy ..\lib\notrealengine\lib\SDL\build-windows\Release\SDL2.dll x64\Release
 )
