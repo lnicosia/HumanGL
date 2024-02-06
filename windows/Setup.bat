2d9c5ceda3fa3bedadf9d36a61b6b88216de6e2d
lnicosia <lnicosia@student.42.fr>
2024-02-06 14:20:54 +0100

    [FIX] Fixing Setup.bat.

--- windows/Setup.bat
+++ windows/Setup.bat
@@ -1,32 +1,32 @@
 @echo off
 SetLocal EnableDelayedExpansion
 
-if not exist lib\notrealengine\.git (
-	git submodule update --init lib/notrealengine
-	)
+if not exist ..\lib\notrealengine\.git (
+	git submodule update --init ..\lib\notrealengine
+)
 
 if not exist SDL2d.dll (
 
-	if not exist lib\SDL\build-windows\Debug\SDL2d.dll (
+	if not exist ..\lib\notrealengine\lib\SDL\build-windows\Debug\SDL2d.dll (
 
-		if not exist lib\SDL\build-windows\SDL2.sln (
+		if not exist ..\lib\notrealengine\lib\SDL\build-windows\SDL2.sln (
 
-			if not exist lib\SDL\include\SDL.h (
-				git submodule update --init lib/SDL
+			if not exist ..\lib\notrealengine\lib\SDL\include\SDL.h (
+				git submodule update --init ..\lib\notrealengine\lib\SDL\.git
 			)
-			if not exist lib\SDL\build-windows (
-				md lib\SDL\build-windows
+			if not exist ..\lib\notrealengine\lib\SDL\build-windows (
+				md ..\lib\notrealengine\lib\SDL\build-windows
 			)
-			cmake -S lib\SDL\ -B lib\SDL\build-windows
+			cmake -S ..\lib\notrealengine\lib\SDL\ -B ..\lib\notrealengine\lib\SDL\build-windows
 			
 		)
-		for /f "delims=" %%i in ('lib\vswhere.exe -version 16 -property installationPath -nologo') do (
+		for /f "delims=" %%i in ('..\lib\vswhere.exe -version 16 -property installationPath -nologo') do (
 			set msbuild-path="%%i\MSBuild\Current\Bin\MSBuild.exe"
 			echo Launching !msbuild-path!...
-			!msbuild-path! lib\SDL\build-windows\SDL2.sln
+			!msbuild-path! ..\lib\notrealengine\lib\SDL\build-windows\SDL2.sln
 		)
 	)
-	move lib\SDL\build-windows\Debug\SDL2d.dll windows\
+	move ..\lib\notrealengine\lib\SDL\build-windows\Debug\SDL2d.dll windows\
 )
 
 if not exist src (
