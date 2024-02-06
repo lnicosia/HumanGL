4ebcdb26dde98cf9554359d74ddf226ff909b928
lnicosia <lnicosia@student.42.fr>
2024-02-06 22:59:40 +0100

    [FIX] Fixed SDL dll copy path.

--- windows/Setup.bat
+++ windows/Setup.bat
@@ -5,7 +5,7 @@ if not exist ..\lib\notrealengine\.git (
 	git submodule update --init --recursive ..\lib\notrealengine
 )
 
-if not exist SDL2d.dll (
+if not exist .\x64\Debug\SDL2d.dll (
 
 	if not exist ..\lib\notrealengine\lib\SDL\build-windows\Debug\SDL2d.dll (
 
@@ -23,7 +23,7 @@ if not exist SDL2d.dll (
 			!msbuild-path! ..\lib\notrealengine\lib\SDL\build-windows\SDL2.sln
 		)
 	)
-	move ..\lib\notrealengine\lib\SDL\build-windows\Debug\SDL2d.dll .\
+	move ..\lib\notrealengine\lib\SDL\build-windows\Debug\SDL2d.dll .\x64\Debug
 )
 
 if not exist src (
